2dca2a92b8e67a0e51355751acc42d023c534c56
Anvar <fort2652@rambler.ru>
2023-08-05 14:18:46 +0300

    feat: eslint check

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,4 @@
 // import { InputElement } from './UI/inputElement';
 
 // export default { InputElement };
-export { default as InputElement } from './UI/inputElement';
+export { default as InputElement } from './UI/InputElement';
