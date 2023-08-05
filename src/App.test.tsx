9b549a6d2802a62dc73de1a1cea4cb3736d5a5cd
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-08-05 19:54:37 +0200

    fix: changing the linter errors

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,15 +4,15 @@ import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 test('renders the app component', () => {
-    render(<App />);
+  render(<App />);
 
-    expect(screen.getByText('Vite + React')).toBeInTheDocument();
+  expect(screen.getByText('Vite + React')).toBeInTheDocument();
 
-    expect(screen.getByText('count is 0')).toBeInTheDocument();
+  expect(screen.getByText('count is 0')).toBeInTheDocument();
 
-    fireEvent.click(screen.getByText('count is 0'));
+  fireEvent.click(screen.getByText('count is 0'));
 
-    expect(screen.getByText('count is 1')).toBeInTheDocument();
+  expect(screen.getByText('count is 1')).toBeInTheDocument();
 
-    expect(screen.getByText('Click on the Vite and React logos to learn more')).toBeInTheDocument();
+  expect(screen.getByText('Click on the Vite and React logos to learn more')).toBeInTheDocument();
 });
