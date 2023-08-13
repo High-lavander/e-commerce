434abd159b18ca1b438d6f172d2e6575dc59c6b8
Tatsiana <kulinkovich56@gmail.com>
2023-08-13 22:27:35 +0200

    feat: create file system

---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+/*export {default as Main} from './main/main';
+export {default as Error404} from './Error404/Error404';
+export {default as About} from './About/About';
+export {default as Contact} from './Contact/Contact';*/
