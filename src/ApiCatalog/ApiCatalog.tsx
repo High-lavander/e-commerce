7ca5ced91b7b2847883e11a4990c654c5f39b79b
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-09-02 23:13:43 +0200

    feat: api requests and data retrieval

---
+++ src/ApiCatalog/ApiCatalog.tsx
@@ -0,0 +1,116 @@
+import { getToken } from '../store/customer';
+
+export default interface IProductElement {
+  id: string;
+  key: string;
+  description: { [key: string]: string };
+  price: number;
+  imageUrl: string;
+  masterData: {
+    current: {
+      categories: [{ typeId: string; id: string }];
+      masterVariant: {
+        prices: {
+          value: {
+            centAmount: number;
+            currencyCode: string;
+          };
+        }[];
+        images: [{ url: string }];
+      };
+      name: {
+        en: string;
+      };
+    };
+  };
+}
+
+export default interface IGetCategory {
+  id: string;
+  key: string;
+  name: {
+    en: string;
+  };
+}
+
+export const getAllProducts = async () => {
+  try {
+    const tokenObject = await getToken();
+    const accessToken = tokenObject.access_token;
+
+    const response = await fetch(
+      `https://api.${process.env.VITE_CTP_API_REGION}.commercetools.com/${process.env.VITE_CTP_PROJECT_KEY}/products`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error while executing the query: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error in receiving products:', error);
+    throw error;
+  }
+};
+
+export const getAllCategories = async () => {
+  try {
+    const tokenObject = await getToken();
+    const accessToken = tokenObject.access_token;
+
+    const response = await fetch(
+      `https://api.${process.env.VITE_CTP_API_REGION}.commercetools.com/${process.env.VITE_CTP_PROJECT_KEY}/categories`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error while executing the query: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error in receiving category:', error);
+    throw error;
+  }
+};
+
+export const categoryFilter = async (categoryId: string) => {
+  try {
+    const tokenObject = await getToken();
+    const accessToken = tokenObject.access_token;
+
+    const response = await fetch(
+      `https://api.${process.env.VITE_CTP_API_REGION}.commercetools.com/${process.env.VITE_CTP_PROJECT_KEY}/product-projections/search?filter=categories.id:"${categoryId}"`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error while executing the query: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error('Error when receiving products by category:', error);
+    throw error;
+  }
+};
