b999757187c6d6743162715a879a3d241c0bda47
Anvar <fort2652@rambler.ru>
2023-09-11 23:04:38 +0300

    feat: bask item quantity

--- src/components/Basket/lineItemComponent.tsx
+++ src/components/Basket/lineItemComponent.tsx
@@ -1,14 +1,66 @@
-import { useEffect, useState } from 'react';
-import type { ILineItem } from '../../store/basket';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { updateBasketById, type ILineItem, CartActionsType } from '../../store/basket';
 import type { IProductDetail } from '../../types/productDetail';
-import { useAppSelector } from '../../store/hooks';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import type { IToken } from '../../store/customer';
 import './LineItemComponent.scss';
+// import { InputElement } from '..';
+// import useInput from '../../hooks/useInput';
 
 type ILineItemsProps = ILineItem;
 export const LineItemComponent = (props: ILineItemsProps) => {
+  const { basket } = useAppSelector((state) => state.basket);
+  const dispatch = useAppDispatch();
   const [productData, setProductData] = useState<IProductDetail>();
+  // const quantity = useInput(String(props.quantity) || '1');
+  const [quantity, setQuantity] = useState(props.quantity);
   const tokenData = useAppSelector((state) => state.customer.tokenData as IToken | null);
+
+  const quantityChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuantity(Number(e.target.value));
+  };
+
+  const incrementQuantity = () => {
+    setQuantity(quantity + 1);
+    changeLineItemQuantity(quantity + 1);
+  };
+
+  const decrementQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+      changeLineItemQuantity(quantity - 1);
+    }
+  };
+
+  const deleteFromBasket = () => {
+    if (basket) {
+      updateBasketById(basket?.id, {
+        version: basket?.version,
+        actions: [
+          {
+            action: CartActionsType.REMOVEITEM,
+            lineItemId: props.id,
+          },
+        ],
+      });
+    }
+  };
+
+  const changeLineItemQuantity = (quantity: number) => {
+    console.log('changeLineItemQuantity', basket?.id);
+    if (basket) {
+      updateBasketById(basket?.id, {
+        version: basket?.version,
+        actions: [
+          {
+            action: CartActionsType.CHANGEQUANTITY,
+            lineItemId: props.id,
+            quantity,
+          },
+        ],
+      })(dispatch);
+    }
+  };
   useEffect(() => {
     const fetchProductData = async () => {
       fetch(
@@ -39,14 +91,35 @@ export const LineItemComponent = (props: ILineItemsProps) => {
         src={productData?.masterData.current.masterVariant.images[0].url}
         alt={productData?.masterData.current.name.en || 'Product image'}
       />
-      <h2 className="line-item__name"></h2>
-      <div className="line-item__price">{props.price.value.centAmount}</div>
+      <h2 className="line-item__name">{productData?.masterData.current.name.en}</h2>
+      <div className="line-item__prices">
+        <div className="line-item__discount-price">{props.price.value.centAmount}</div>
+        <div className="line-item__price">
+          {props?.discountedPricePerQuantity[0] &&
+            props?.discountedPricePerQuantity?.[0].discountedPrice.value.centAmount}
+        </div>
+      </div>
       <div className="line-item__quantity-container">
-        <button className="line-item__quantity-button quantity-button quantity-button__add">+</button>
-        <div className="line-item__count">{props.quantity}</div>
-        <button className="line-item__quantity-button  quantity-button quantity-button__remove">-</button>
+        <button
+          className="line-item__quantity-button quantity-button quantity-button__increment"
+          onClick={incrementQuantity}
+        >
+          +
+        </button>
+        <div className="line-item__count">
+          <input value={quantity} onChange={quantityChange} className="line-item__quantity-input" type="number" />
+        </div>
+        <button
+          className="line-item__quantity-button  quantity-button quantity-button__decrement"
+          onClick={decrementQuantity}
+        >
+          -
+        </button>
       </div>
       <div className="line-item__total">{props.totalPrice.centAmount}</div>
+      <button className="line-item__quantity-button quantity-button__delete" onClick={deleteFromBasket}>
+        x
+      </button>
     </div>
   );
 };
